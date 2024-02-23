2f188d2bafa1b4d57e2f316552ac47beb4067424
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-23 23:10:47 +0100

    Add mongo connection with cache Changes to be committed: 	new file:   lib/database/mongoose.ts 	modified:   package-lock.json 	modified:   package.json

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,34 @@
+import mongoose, { Mongoose } from 'mongoose'
+
+const MONGODB_URL = process.env.MONGODB_URL
+
+interface MongooseConnection {
+  connected: Mongoose | null
+  promise: Promise<Mongoose> | null
+}
+
+let cached: MongooseConnection = (global as any).mongoose
+console.log(1, global)
+if (!cached) {
+  cached = (global as any).mongoose = {
+    connected: null,
+    promise: null,
+  }
+}
+
+export const connectToDatabase = async () => {
+  if (cached.connected) return cached.connected
+
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL')
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: 'imaginify',
+      bufferCommands: false,
+    })
+
+  cached.connected = await cached.promise
+
+  return cached.connected
+}
