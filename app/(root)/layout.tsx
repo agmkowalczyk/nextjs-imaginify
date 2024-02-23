ec1cac7dd7582ba479b525895bd1a70d9435fc88
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-23 22:24:23 +0100

    Add Sidebar Changes to be committed: 	modified:   app/(root)/layout.tsx 	new file:   components/shared/Sidebar.tsx 	new file:   components/ui/button.tsx 	new file:   constants/index.ts 	modified:   package-lock.json 	modified:   package.json

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
+import MobileNav from '@/components/shared/MobileNav'
+import Sidebar from '@/components/shared/Sidebar'
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className='root'>
+      <Sidebar />
       <div className='root-container'>
         <div className='wrapper'>{children}</div>
       </div>
