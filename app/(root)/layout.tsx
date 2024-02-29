66d201b44ea6e2a370c5180cf966a6e529fd4694
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-02-29 12:43:52 +0100

    Implement uploading image Changes to be committed: 	modified:   app/(root)/layout.tsx 	modified:   components/shared/MediaUploader.tsx 	modified:   lib/utils.ts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -9,6 +10,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <div className='root-container'>
         <div className='wrapper'>{children}</div>
       </div>
+      <Toaster />
     </main>
   )
 }
--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,5 +1,10 @@
+'use client'
+
+import Image from 'next/image'
 import { useToast } from '../ui/use-toast'
-import { CldUploadWidget } from 'next-cloudinary'
+import { CldImage, CldUploadWidget } from 'next-cloudinary'
+import { dataUrl, getImageSize } from '@/lib/utils'
+import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
 
 type MediaUploaderProps = {
   onValueChange: (value: string) => void
@@ -19,6 +24,16 @@ const MediaUploader = ({
   const { toast } = useToast()
 
   const onUploadSuccessHandler = (result: any) => {
+    setImage((prevState: any) => ({
+      ...prevState,
+      publicId: result?.info?.public_id,
+      width: result?.info?.width,
+      height: result?.info?.height,
+      secureURL: result?.info?.secure_url,
+    }))
+
+    onValueChange(result?.info?.public_id)
+
     toast({
       title: 'Image uploaded successfully',
       description: '1 credit was deducted from your account',
@@ -45,7 +60,41 @@ const MediaUploader = ({
       }}
       onSuccess={onUploadSuccessHandler}
       onError={onUploadErrorHandler}
-    ></CldUploadWidget>
+    >
+      {({ open }) => (
+        <div className='flex flex-col gap-4'>
+          <h3 className='h3-bold text-dark-600'>Original</h3>
+
+          {publicId ? (
+            <>
+              <div className='cursor-pointer overflow-hidden rounded-[10px]'>
+                <CldImage
+                  width={getImageSize(type, image, 'width')}
+                  height={getImageSize(type, image, 'height')}
+                  src={publicId}
+                  alt='image'
+                  sizes={'(max-width: 767px) 100vw, 50vw'}
+                  placeholder={dataUrl as PlaceholderValue}
+                  className='media-uploader_cldImage'
+                />
+              </div>
+            </>
+          ) : (
+            <div className='media-uploader_cta' onClick={() => open()}>
+              <div className='media-uploader_cta-image'>
+                <Image
+                  src='/assets/icons/add.svg'
+                  alt='Add Image'
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className='p-14-medium'>Click here to upload image</p>
+            </div>
+          )}
+        </div>
+      )}
+    </CldUploadWidget>
   )
 }
 
