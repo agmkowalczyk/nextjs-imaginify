3f0522dce62691dad59ed09ecd9fb315a83ee9e1
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-03-18 23:06:11 +0100

    Transaction actions Changes to be committed: 	new file:   lib/actions/transaction.actions.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   types/index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -98,3 +98,19 @@ declare type RemoveUrlQueryParams = {
   searchParams: string
   keysToRemove: string[]
 }
+
+declare type CheckoutTransactionParams = {
+  plan: string
+  credits: number
+  amount: number
+  buyerId: string
+}
+
+declare type CreateTransactionParams = {
+  stripeId: string
+  amount: number
+  credits: number
+  plan: string
+  buyerId: string
+  createdAt: Date
+}
